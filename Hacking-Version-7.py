eee1d22a417ee6120c570af4a774f9d37bf69c77
Bassem-13 <71402157+Bassem-13@users.noreply.github.com>
2020-09-16 20:36:12 +0200

    Update Hacking-Version-7.py

--- Hacking-Version-7.py
+++ Hacking-Version-7.py
@@ -1,4 +1,4 @@
-# Hacking Version 2
+# Hacking Version 7
 # This is a text-based password guessing game that displays a list of potential
 # computer passwords. The player is allowed 1 attempt to guess the password. 
 # The game indicates that the player failed to guess the password correctly. 
